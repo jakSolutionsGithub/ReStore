fa5f8e6b4ce423b71d63f791c3eb4960c9788207
korpinz <45260016+korpinz@users.noreply.github.com>
2022-03-10 16:37:48 +0100

    Card list of products

---
+++ src/app/layout/App.tsx
@@ -0,0 +1,17 @@
+import { Container, CssBaseline } from "@mui/material";
+import Catalog from "../../features/catalog/Catalog";
+import Header from "./Header";
+
+function App() {
+    return (
+        <>
+            <CssBaseline />
+            <Header />
+            <Container>
+                <Catalog />
+            </Container>
+        </>
+    );
+}
+
+export default App;
---
+++ src/features/catalog/ProductCard.tsx
@@ -0,0 +1,51 @@
+import {
+    Button,
+    Card,
+    CardActions,
+    CardContent,
+    CardHeader,
+    CardMedia,
+    Typography,
+} from "@mui/material";
+import { Product } from "../../app/models/product";
+
+interface Props {
+    product: Product;
+}
+
+export default function ProductCard({ product }: Props) {
+    return (
+        <Card>
+            <CardHeader
+                title={product.name}
+                titleTypographyProps={{
+                    sx: {
+                        fontWeight: "bold",
+                        color: "primary.main",
+                        fontSize: 16,
+                    },
+                }}
+            ></CardHeader>
+            <CardMedia
+                component="img"
+                height="140"
+                sx={{ backgroundSize: "contain" }}
+                image={product.pictureUrl}
+                title={product.name}
+                alt={product.name}
+            />
+            <CardContent>
+                <Typography gutterBottom color="secondary" variant="h5">
+                    {(product.price / 100).toFixed(2)} €
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                    {product.brand} / {product.type}
+                </Typography>
+            </CardContent>
+            <CardActions>
+                <Button size="small">Add to card</Button>
+                <Button size="small">View</Button>
+            </CardActions>
+        </Card>
+    );
+}
